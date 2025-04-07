4e22e3c015ca268b903c9e99a9b1bc36040dc3c1
Gennady Paleev <gv.paleev@gmail.com>
2025-04-07 23:09:47 +0100

    tasl 7

--- main.rs
+++ main.rs
@@ -1,21 +1,21 @@
-// Дается некий диапазон целых чисел, например от 17 до 24 включительно.
-// Напишите программу, которая будет находить сумму целых чисел указанного диапазона.
+// Есть некий код:
+//
+// fn main() {
+//     let mut n: i8 = 14;
+//     while n >= 0 {
+//         n -= 5;
+//         println!("{n}");
+//     }
+// }
+// Доработайте эту программу, чтобы она распечатала остаток от деления n на 5. Не используйте оператор %.
+//
 
-use rand::Rng;
+// use rand::Rng;
 
 fn main() {
-    let mut rng = rand::rng();
-
-    let arr = [
-        rng.random_range(17..25),
-        rng.random_range(17..25),
-        rng.random_range(17..25),
-        rng.random_range(17..25),
-        rng.random_range(17..25),
-        rng.random_range(17..25),
-        rng.random_range(17..25),
-    ];
-
-    let sum: i32 = arr.iter().sum();
-    println!("Для массива {:?}, сумма равна {}", arr, sum);
+    let mut n: i8 = 14;
+    while n - 5 >= 0 {
+        n -= 5;
+    }
+    println!("{n}");
 }
