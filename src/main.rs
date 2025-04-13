7668644ddeb7628ce3d0791ee093379a67e38ab5
Gennady Paleev <gv.paleev@gmail.com>
2025-04-13 22:12:14 +0100

    sask 17

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+    let arr = [
+        "(){}[]".to_string(),
+        "([{}])".to_string(),
+        "(}".to_string(),
+        "[(])".to_string(),
+        "[({})](]".to_string(),
+    ];
+
+    for str in arr {
+        println!("{} => {}", str, valid_braces(&str))
+    }
+}
+
+fn valid_braces(s: &str) -> bool {
+    let mut stack = Vec::new();
+
+    for ch in s.chars() {
+        match ch {
+            '(' | '[' | '{' => stack.push(ch),
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => return false,
+        }
+    }
+
+    stack.is_empty()
 }
